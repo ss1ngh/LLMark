c18016a93ba145f3e5246c1611e7b4def1de2feb
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-16 13:53:15 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     crx({ manifest }),
   ],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    cors: {
+      origin: "*", // Allow all origins (including chrome-extension://...)
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    }
+  },
 })
